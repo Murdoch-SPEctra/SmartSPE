5fcc004e7bee5c9caaf50ecbeb4a90f958adc234
Gilchrist <67920312+Gilospy@users.noreply.github.com>
2025-10-22 00:49:52 +0400

    Test data

---
+++ tests/attempt_form.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'userid' => 5,
+    'speid' => 1,
+    'spe_name' => 'SPE 1',
+    'cmid' => 10,
+    'group' => (array)[
+        'id' => 1,
+        'name' => 'SPEctra',
+        'members' => [
+            5 => (object) ['id' => 5, 'fullname' => 'Tercia Fernandes'],
+            6 => (object) ['id' => 6, 'fullname' => 'Khushank Jain'],
+            7 => (object) ['id' => 7, 'fullname' => 'Meagan Saldanha'],
+            4 => (object) ['id' => 4, 'fullname' => 'Rabiya Saleh'],
+        ]
+    ],
+    'questions' => [
+        101 => (object) ['id' => 101, 'text' => 'Teamwork', 'sort_order' => 1],
+        102 => (object) ['id' => 102, 'text' => 'Communication', 'sort_order' => 2]
+    ]
+];
---
+++ tests/attempt_form_submit.php
@@ -0,0 +1,61 @@
+<?php
+// The format
+// // is [
+//     'rating' => [
+//         evaluated_userid => [
+//             questionid => ratingvalue (1-5)
+//         ]
+//     ],
+//     'comment' => [
+//         evaluated_userid => 'text comment'
+//     ],
+//     'selfreflect' => 'text self reflection',
+//     'submitbutton' => 'Submit Evaluation'
+// ]
+return (object)[
+    ['rating'] => Array
+        (
+            3 => Array
+                (
+                    5 => 2
+                    6 => 2
+                )
+
+            5 => Array
+                (
+                    5 => 2
+                    6 => 3
+                )
+
+            6 => Array
+                (
+                    5 => 3
+                    6 => 4
+                )
+
+            7 => Array
+                (
+                    5 => 4
+                    6 => 2
+                )
+
+            4 => Array
+                (
+                    5 => 5
+                    6 => 2
+                )
+
+        )
+
+    'comment' => Array
+        (
+            3 => sadsadsadadad
+            5 => sadadadadadsad
+            6 => sadsadsadadsadsa
+            7 => asdadadadadad
+            4 => dasdsadsadsad
+        )
+
+    'selfreflect' => adadadadadasadad
+    'submitbutton' => Submit Evaluation
+]
