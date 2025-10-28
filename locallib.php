bd1a7a5df101276285acd3df356fc63dfd03fa3a
Gilchrist <67920312+Gilospy@users.noreply.github.com>
2025-10-28 20:15:12 +0400

    Add function to generate random comment

--- locallib.php
+++ locallib.php
@@ -60,8 +60,6 @@ function smartspe_get_sentiment_batch($comments) {
         if (json_last_error() !== JSON_ERROR_NONE) {
             throw new Exception('Invalid JSON from sentiment API');
         }
-
-        // $result should be: 0 => sentiment, 1 => sentiment, etc.
         return $result;
 
     } catch (\Throwable $e) {
@@ -78,4 +76,30 @@ function smartspe_get_studentid_from_email(string $email): string {
         return 'Unknown ID';
     }
     return substr($email, 0, $pos);
+}
+
+
+
+function smartspe_get_random_comment(): string {
+    $comments = [
+         "Very reliable teammate.",
+        "Needs improvement on deadlines.",
+        "Great communication and effort.",
+        "Strong technical skills, helpful to the group.",
+        "Could participate more actively.",
+        "Z excelled as the team lead, effectively coordinating tasks and ensuring smooth collaboration among all members. She created well-structured and interactive visuals and documentation, which greatly improved the quality of our deliverables.
+            She also worked closely with M to review and fix workflows when issues arose, ensuring that the system functioned properly, spending a great amount of time troubleshooting. Z took the lead in presenting our project during client meetings and weekly discussions, and she was always willing to help troubleshoot any problems the team encountered, providing support that kept the project moving forward.",
+        
+        "Showed very good project management skills as she was the project activity coordinator.
+        Regularly took updates about the various tasks of the project, including the development side and the documentation part.
+        Exhibited excellent communication skills throughout and ensured to keep the project team members motivated and on track.
+        Involved in the testing phase.
+        Submitted all documents on time with proper proofreading and formatting.",
+
+        "N was the Secretary and Librarian. As the Secretary, she was responsible for creating meeting agendas before meetings and meeting minutes after the meetings with the supervisor and client. And she also managed a repository (Google Groups) for the meeting agendas and meeting minutes. As the Librarian, she was responsible for maintaining a standardised file naming standard, a repository for all project documentation. For the technical side of the project, she was responsible for creating the dashboard for the SOC/admin and developing the database for storing the output given from the scraper tool.",
+    
+        "T contributed meaningfully to both the technical development and documentation areas of the project. He wrote detailed sections of the PMP and supported the technical team when needed, demonstrating versatility and a solid understanding of both areas. Although he had commitments in other units that occasionally limited his availability, he consistently demonstrated a strong work ethic and remained reliable in completing his tasks.",
+    ];
+
+    return $comments[array_rand($comments)];
 }
